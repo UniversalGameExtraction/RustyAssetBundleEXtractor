17afde7a5e229cc360b38bac0dcafaad06c327c3
Rudolf Kolbe <rkolbe96@gmail.com>
2023-06-24 00:09:57 +0200

    ExtractionConfig - make new public and add default

--- src/config.rs
+++ src/config.rs
@@ -4,15 +4,24 @@ pub struct ExtractionConfig {
 }
 
 impl ExtractionConfig {
-    pub(crate) fn new() -> Self {
+    pub fn new(unitycn_key: Option<[u8; 16]>, fallback_unity_version: String) -> Self {
         Self {
-            unitycn_key: None,
-            fallback_unity_version: "2.5.0f5".to_owned(),
+            unitycn_key,
+            fallback_unity_version,
         }
     }
 
     pub fn with_unitycn_key(mut self, key: [u8; 16]) -> Self {
         self.unitycn_key = Some(key);
         self
+    }
+}
+
+impl Default for ExtractionConfig {
+    fn default() -> Self {
+        Self {
+            unitycn_key: None,
+            fallback_unity_version: "2.5.0f5".to_owned(),
+        }
     }
 }
